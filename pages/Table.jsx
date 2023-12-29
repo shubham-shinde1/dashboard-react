1c829b47a7e4bf24bb0081316900391bfb9c3a3d
shubham-shinde1 <shubhamshinde308@gmil.com>
2023-12-29 10:12:44 +0530

    optimized

--- pages/Table.jsx
+++ pages/Table.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 export default function Table() {
   return (
-    <div className="relative overflow-x-auto ">
+    <div className="relative overflow-x-hidden border rounded ">
+      <div className="p-5 text-xs font-semibold  text-gray-500 bg-white flex justify-between">
+        <div>PEOPLE LEADERBOARD</div>
+        <button className="text-blue-500">All People</button>
+      </div>
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
-        <caption className="p-5 text-lg font-bold text-left rtl:text-right text-gray-600 bg-white dark:text-white dark:bg-gray-800 ">
-          PEOPLE LEADERBOARD
-        </caption>
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-100 ">
           <tr>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-6 py-3 ">
               Name
             </th>
             <th scope="col" className="px-6 py-3">
@@ -21,40 +22,40 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b  ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap "
             >
               Lizzie Rose
             </th>
             <td className="px-6 py-4">5</td>
             <td className="px-6 py-4">36</td>
           </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap "
             >
               David Buttler
             </th>
             <td className="px-6 py-4">5</td>
             <td className="px-6 py-4">36</td>
           </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap "
             >
               Nick Evans
             </th>
             <td className="px-6 py-4">5</td>
             <td className="px-6 py-4">36</td>
           </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <tr className="bg-white border-b  ">
             <th
               scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap "
             >
               Mariline Lubin
             </th>
